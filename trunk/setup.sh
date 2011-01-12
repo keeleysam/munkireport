fb576b84bdf255a1d1fe89dc7290b4fcdeaad9a3
pelle <pelle@19f8b892-4368-c07f-3fa0-c405c512c942>
2011-01-12 20:26:51 +0000

    Added setup stubs.

--- trunk/setup.sh
+++ trunk/setup.sh
@@ -3,3 +3,21 @@
 
 echo "Unimplemented, refer to README.txt"
 exit 1
+
+
+if [ -e production.db ]; then
+    echo "A production database already exists. Please refer to the documentation if you"
+    echo "are upgrading to a new version."
+    exit 1
+fi
+
+
+# Create virtualenv
+
+# Create production.ini
+
+# Create users
+
+# Create groups.ini
+
+# Create permissions.init
