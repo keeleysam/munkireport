04e42ef57fc59e9f148f390d03fd565784c2cbeb
MagerValp@gmail.com <MagerValp@gmail.com@19f8b892-4368-c07f-3fa0-c405c512c942>
2011-02-11 23:53:14 +0000

    Distribution now creates a dmg with a pkg that installs into /Library/MunkiReport.

--- trunk/Setup/make_pkg.sh
+++ trunk/Setup/make_pkg.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
 
-# Check for virtualenv
-if [ -z "$VIRTUAL_ENV" ]; then
-    echo "Activate the virtualenv before running this script."
+SCRIPTDIR=`dirname "$0"`
+SRCDIR=`cd "$SCRIPTDIR/.."; pwd`
+MRDIR="/Library/MunkiReport"
+VENVDIR="$MRDIR/Python"
+PKGID="com.googlecode.pkg.MunkiReport"
+
+# Sanity checks.
+if [ ! -x "/Developer/usr/bin/packagemaker" ]; then
+    echo "PackageMaker is not installed!"
+    exit 1
+fi
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Package creation needs to be run as root"
+    exit
+fi
+
+if [ -z "$1" ]; then
+    echo "Please provide .egg as argument"
+    exit 1
+else
+    EGG="$1"
+fi
+
+if [ -e "$MRDIR" ]; then
+    echo "$MRDIR already exists"
     exit 1
 fi
 
@@ -12,49 +35,114 @@ fi
 VERSION=`python -c 'import munkireport; print munkireport.__version__'`
 SVNREV=`svnversion . | cut -d: -f2 | tr -cd '[:digit:]'`
 
+PKG="$SRCDIR/dist/MunkiReport-$VERSION.$SVNREV.pkg"
 
-# Build distribution egg
-rm -rf build dist
-python setup.py --quiet bdist_egg
+
+# Create a PackageInfo.
+makeinfo() {
+    out="$1"
+    id="$2"
+    ver="$3"
+    size="$4"
+    nfiles="$5"
+    restart="$6"
+    if [ "$restart" == "restart" ]; then
+        restart=' postinstall-action="restart"' # Leading space is important.
+    else
+        restart=""
+    fi
+    cat > "$out" <<EOF
+<pkg-info format-version="2" identifier="$id" version="$ver" install-location="/" auth="root"$restart>
+    <payload installKBytes="$size" numberOfFiles="$nfiles"/>
+</pkg-info>
+EOF
+}
+
+
+# Create directory structure.
+mkdir "$MRDIR"
+mkdir "$MRDIR"/Python
+rsync -rlptC "$SRCDIR/bin" "$MRDIR/"
+rsync -rlptC "$SRCDIR/Scripts" "$MRDIR/"
+
+# Create virtualenv
+echo "* Creating virtual environment"
+(
+    cd "$MRDIR"
+    virtualenv --no-site-packages -p python2.6 `basename "$VENVDIR"`
+    if [ $? -ne 0 ]; then echo "virtual environment creation failed"; exit 1; fi
+)
+echo "* Activating virtual environment"
+source "$VENVDIR/bin/activate"
+if [ $? -ne 0 ]; then echo "virtual environment activation failed"; exit 1; fi
+
+# Install egg
+echo "* Installing MunkiReport into virtual environment"
+easy_install --quiet "$EGG"
 if [ $? -ne 0 ]; then
-    echo "Egg build failed"
+    echo
+    echo "Install failed."
     exit 1
 fi
-rm -rf build
+
+# Set modes.
+find "$MRDIR" -print0 | xargs -0 chmod u+rw,go-w
 
 
-# Create distribution template
-ROOTDIR="dist/MunkiReport-$VERSION.$SVNREV"
-mkdir "$ROOTDIR"
-mkdir "$ROOTDIR/Library"
-mkdir "$ROOTDIR/Library/MunkiReport"
-DISTDIR="$ROOTDIR/Library/MunkiReport"
-mv dist/*.egg "$DISTDIR/"
-rsync -rlptC bin "$DISTDIR/"
-rsync -rlptC Scripts "$DISTDIR/"
-mkdir "$DISTDIR/etc"
-mkdir "$DISTDIR/var"
-mkdir "$DISTDIR/var/db"
-mkdir "$DISTDIR/var/log"
-mkdir "$DISTDIR/var/run"
-cp etc/production.ini.template etc/permissions.ini "$DISTDIR/etc/"
-cp setup.sh shell.sh start.sh "$DISTDIR/"
-cp README.txt LICENSE.txt "$DISTDIR/"
-cp com.googlecode.munkireport.plist "$DISTDIR/"
+# Pre-build cleanup.
+rm -rf "$PKG"
 
+# Create temporary directory
+PKGTMP=`mktemp -d -t mrpkg`
+
+
+echo "* Creating package template"
+
+
+# Create directory structure.
+PKGROOT="$PKGTMP/munkireport"
+mkdir -m 1775 "$PKGROOT"
+mkdir -m 1775 "$PKGROOT/Library"
+mkdir -m 0755 "$PKGROOT/Library/LaunchDaemons"
+cp "$SCRIPTDIR/com.googlecode.munkireport.plist" "$PKGROOT/Library/LaunchDaemons"
+
+# Move MunkiReport into package root.
+mv /Library/MunkiReport "$PKGROOT/Library"
+
+# Copy ini file template.
+cp "$SCRIPTDIR/MunkiReport.ini.template" "$PKGROOT/Library/MunkiReport"
+
+# Create package info file.
+PKGSIZE=`du -sk $PKGROOT | cut -f1`
+NFILES=$(echo `find $PKGROOT/ | wc -l`)
+makeinfo "$PKGTMP/info" "$PKGID" "$VERSION" $PKGSIZE $NFILES norestart
 
 # Clean up
-find dist -name '.DS_Store' -exec rm {} \;
-xattr -d -r com.apple.FinderInfo dist
-xattr -d -r com.macromates.caret dist
+find "$PKGROOT" -name '.DS_Store' -exec rm {} \;
+xattr -d -r com.apple.FinderInfo "$PKGROOT"
+xattr -d -r com.macromates.caret "$PKGROOT"
+
+# Set ownership.
+chown -hR root:admin "$PKGROOT"
+chown -hR root:wheel "$PKGROOT/Library/LaunchDaemons"
 
 
-# Create archive
-(
-    cd dist
-    tar jcf "MunkiReport-$VERSION.$SVNREV.tar.bz2" "MunkiReport-$VERSION.$SVNREV"
-)
+echo "* Packaging $PKG"
+/Developer/usr/bin/packagemaker \
+    --root "$PKGROOT" \
+    --info "$PKGTMP/info" \
+    --resources "$SCRIPTDIR/PkgTemplate/Resources" \
+    --scripts "$SCRIPTDIR/PkgTemplate/Scripts" \
+    --id "$PKGID" \
+    --version "$VERSION" \
+    --no-recommend \
+    --no-relocate \
+    --target 10.5 \
+    --out "$PKG" \
+    #--verbose
 
 
-# Done
-open dist
+echo "* Removing temporary files..."
+sudo rm -rf "$PKGTMP"
+
+echo "Done."
